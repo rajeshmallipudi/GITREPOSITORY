39d7123c7fab6adb8238a0fff8bf8f217f8b3fd9
rajesh <rajesh@host.docker.internal>
2020-07-20 14:35:53 +0530

    GIT COMMIT

---
+++ GITREPOSITORY/src/main/java/GITREPOSITORY/GITREPOSITORY/App.java
@@ -0,0 +1,13 @@
+package GITREPOSITORY.GITREPOSITORY;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
